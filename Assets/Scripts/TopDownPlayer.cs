c47f0c1b16d06b37993d4c5fc3ab1000e2c45f23
OuterFrog <91077751+OuterFrog@user.noreply.github.com>
2025-11-22 00:48:31 +0100

    Top down rotation

--- Assets/Scripts/TopDownPlayer.cs
+++ Assets/Scripts/TopDownPlayer.cs
@@ -7,11 +7,17 @@ public class TopDownPlayer : MonoBehaviour
 
     [SerializeField, Range(0, 500)] float speed;
 
+    [SerializeField] float rotSpeed;
+
     Rigidbody rb;
+    Transform visualTransform;
+
+    Quaternion targetRot = Quaternion.Euler(0,0,0);
 
     void Awake()
     {
         rb = GetComponent<Rigidbody>();
+        visualTransform = transform.GetChild(0);
     }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -22,7 +28,12 @@ public class TopDownPlayer : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
+    {   
+        Vector2 inputVector = moveInput.action.ReadValue<Vector2>();
+        if(inputVector.magnitude > 0)
+        {
+            visualTransform.rotation = Quaternion.Lerp(visualTransform.rotation, targetRot, rotSpeed * Time.deltaTime);
+        }
     }
 
     void FixedUpdate()
@@ -30,6 +41,13 @@ public class TopDownPlayer : MonoBehaviour
         Vector2 inputVector = moveInput.action.ReadValue<Vector2>();
         Vector3 dirVector = new Vector3(inputVector.x, 0, inputVector.y);
         rb.linearVelocity = speed * dirVector * Time.fixedDeltaTime;
+
+        if(rb.linearVelocity.magnitude > 0)
+        {
+            Vector2 dir = new Vector2(rb.linearVelocity.x, rb.linearVelocity.z).normalized;
+            float rot = Mathf.Atan2(dir.x, dir.y) * Mathf.Rad2Deg;
+            targetRot = Quaternion.Euler(0,rot,0);
+        }
     }
 
     void OnTriggerEnter(Collider other)
