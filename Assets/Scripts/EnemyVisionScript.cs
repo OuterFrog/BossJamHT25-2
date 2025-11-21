3318e74b8b258b3e42e85445a93d155b3b636a7d
DagOhlin <ohlin.dag@gmail.com>
2025-11-21 23:14:50 +0100

    added to visionscript

--- Assets/Scripts/EnemyVisionScript.cs
+++ Assets/Scripts/EnemyVisionScript.cs
@@ -18,6 +18,29 @@ public class EnemyVisionScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (GameManager.GetPlayerObj() != null) {
+            lookFor(GameManager.GetPlayerObj());
+        }
+        else Debug.Log("no playerref");
+    }
+
+
+    void lookFor(GameObject player)
+    {
+        RaycastHit hit;
+
+        Vector3 direction = player.transform.forward - transform.forward;
+
+        Debug.DrawRay(transform.position, direction, Color.red);
+
+        if(Physics.Raycast(transform.position, direction, out hit, viewRange))
+        {
+            if (Vector3.Dot(direction, transform.forward) < viewRange || Vector3.Dot(direction, transform.forward) > -1 * viewRange) {
+
+                Debug.Log("i can see you");
+
+            }
+            
+        }
     }
 }
