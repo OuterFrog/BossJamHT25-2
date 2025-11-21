619078f46425a514292b2a673b3f57fac8561d10
Gabriel <gabriel.abr.wik@outlook.com>
2025-11-21 21:54:54 +0100

    working 1st person movement

--- Assets/Scripts/FPPlayer.cs
+++ Assets/Scripts/FPPlayer.cs
@@ -4,29 +4,42 @@ using UnityEngine.InputSystem;
 public class FPPlayer : MonoBehaviour
 {
     public float walkSpeed;
-    public Vector2 inputVector;
+    public float mouseSensitivity;
+    public Vector2 walkVector;
+    public Vector2 mouseVector;
     public Rigidbody rig;
     public Camera cam;
 
-    public InputActionReference inputAction;
+    float xRotation = 0;
+
+    public InputActionReference moveInput;
+    public InputActionReference mouseInput;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
     void Update()
     {
-        inputVector = inputAction.action.ReadValue<Vector2>();
+        walkVector = moveInput.action.ReadValue<Vector2>();
+        mouseVector = mouseInput.action.ReadValue<Vector2>() * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseVector.y;
+        xRotation = Mathf.Clamp(xRotation, -90.0f, 90.0f);
+
+        cam.transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
+        transform.Rotate(Vector3.up * mouseVector.x);
     }
 
     private void FixedUpdate()
     {
         Vector3 moveVector = new Vector3(0, rig.linearVelocity.y, 0);
-        moveVector.x = inputVector.x * cam.transform.right.x * walkSpeed * Time.deltaTime;
-        moveVector.z = inputVector.y * cam.transform.forward.z * walkSpeed * Time.deltaTime;
+        moveVector += walkVector.x * transform.right;
+        moveVector += walkVector.y * transform.forward;
+        moveVector = moveVector.normalized * walkSpeed * Time.fixedDeltaTime;
 
         rig.linearVelocity = moveVector;
     }
