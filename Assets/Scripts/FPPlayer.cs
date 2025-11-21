fbd80b8465bf54536739f6ea23815208249188ea
Gabriel <gabriel.abr.wik@outlook.com>
2025-11-21 19:36:57 +0100

    added scripts folder

--- Assets/Scripts/FPPlayer.cs
+++ Assets/Scripts/FPPlayer.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class TestFest : MonoBehaviour
+public class FPPlayer : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
