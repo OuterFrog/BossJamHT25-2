119b64d9e1ce6da5ef7b3a83bc0746ad02cf5704
Gabriel <gabriel.abr.wik@outlook.com>
2025-11-21 21:17:04 +0100

    mouse input actions n' stuff

--- Assets/Scripts/FPPlayer.cs
+++ Assets/Scripts/FPPlayer.cs
@@ -1,7 +1,15 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class FPPlayer : MonoBehaviour
 {
+    public float walkSpeed;
+    public Vector2 inputVector;
+    public Rigidbody rig;
+    public Camera cam;
+
+    public InputActionReference inputAction;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +19,15 @@ public class FPPlayer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        inputVector = inputAction.action.ReadValue<Vector2>();
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 moveVector = new Vector3(0, rig.linearVelocity.y, 0);
+        moveVector.x = inputVector.x * cam.transform.right.x * walkSpeed * Time.deltaTime;
+        moveVector.z = inputVector.y * cam.transform.forward.z * walkSpeed * Time.deltaTime;
+
+        rig.linearVelocity = moveVector;
     }
 }
